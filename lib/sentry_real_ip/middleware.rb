bee6d62284ec460eb4931337e2f2e992ed139e56
Jan Sandbrink <jan.sandbrink@kaeuferportal.de>
2016-09-16 15:21:53 +0200

    Extract method

--- lib/sentry_real_ip/middleware.rb
+++ lib/sentry_real_ip/middleware.rb
@@ -7,13 +7,19 @@ module SentryRealIp
     end
 
     def call(env)
-      real_ip = env['HTTP_X_FORWARDED_FOR'] || env['HTTP_X_REAL_IP']
-      if real_ip && !real_ip.empty?
-        real_ip = real_ip.split(',').first.strip
+      real_ip = determine_real_ip
+      if real_ip
         user_context = { ip_address: real_ip }.merge(Raven.context.user)
         Raven.user_context(user_context)
       end
       @app.call(env)
+    end
+
+    private
+
+    def determine_real_ip
+      real_ip = env['HTTP_X_FORWARDED_FOR'] || env['HTTP_X_REAL_IP']
+      real_ip.split(',').first.strip if real_ip && !real_ip.empty?
     end
   end
 end
